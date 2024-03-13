2eba7a36e5404ae5b96c592b62986e9be57f4b58
Luna Lopis <lunalopis59@gmail.com>
2024-03-13 12:06:18 +0100

    add migrations

---
+++ database/migrations/2024_03_13_091530_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->text('description');
+            $table->string('image');
+            $table->char('ean_code', 13);
+            $table->double('price', 5,2);
+            $table->boolean('in_evidence');
+            $table->date('last_edit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
