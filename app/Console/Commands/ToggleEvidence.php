982c6a65ad7e0c6908ffbf617f616685f4cfbc7c
RodiaGiordano <rodiagiordano93@gmail.com>
2024-03-21 20:07:59 +0100

    add promtformissing argument for artisan product:evidence

--- app/Console/Commands/ToggleEvidence.php
+++ app/Console/Commands/ToggleEvidence.php
@@ -2,10 +2,11 @@
 
 namespace App\Console\Commands;
 use App\Models\Product;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
 
 use Illuminate\Console\Command;
 
-class ToggleEvidence extends Command
+class ToggleEvidence extends Command implements PromptsForMissingInput
 {
     /**
      * The name and signature of the console command.
@@ -26,6 +27,32 @@ class ToggleEvidence extends Command
      *
      * @return int
      */
+    // public function handle()
+    // {
+    //     $product = Product::find($this->argument('id'));
+      
+    //     if($product){
+
+    //         $product->in_evidence = $product->in_evidence == true ? false : true;
+            
+    //         $message = $product->in_evidence == false ? "è stato nascosto!" : "è stato messo in evidenza!";
+            
+    //         $this->info("Il product con id:$product->id $message");
+    //         $product->save();
+            
+    //     }else{
+    //         $this->error("Il product con id:{$this->argument('id')} non è stato trovato" );
+            
+    //     }
+    // }
+
+    protected function promptForMissingArgumentsUsing(): array
+{
+    return [
+        'id' => 'Specifica un product ID',
+    ];
+}
+
     public function handle()
     {
         $product = Product::find($this->argument('id'));
@@ -44,4 +71,5 @@ class ToggleEvidence extends Command
             
         }
     }
+
 }
